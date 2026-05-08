0ef06013122e85897e82a2523a29e54d31ebf3dc
anri <anriskr14@gmail.com>
2026-05-08 04:05:33 +0400

    fix

--- app/(main)/news/page.tsx
+++ app/(main)/news/page.tsx
@@ -1,35 +1,24 @@
-
+import pool from "@/lib/db"
 import Newslayout from "@/components/Newslayout"
 import NewsTitleLayout from "@/components/NewsTitleLayout"
 import TwoBigCardLayout from "@/components/TwoBigCardLayout"
 import { gg } from "@/lib/testData"
 
-
-
-
 const news = async () => {
-
-  
-const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`);
-const newsPosts = await res.json()
-
+  const result = await pool.query(
+    'SELECT id, slug, image_url, title, tags, short_desc, post_content, category, created_at FROM newsposts WHERE slider_status = false ORDER BY created_at DESC'
+  )
+  const newsPosts = result.rows
 
   return (
     <>
-
-<section className="lg:ml-33 max-w-420  mb-5 px-5">
-
-<NewsTitleLayout title = "ახალი ამბები"  desc = "ყველაფერი, რაც გეიმერს აინტერესებს — ახალი ამბები, ტექნიკა, ჭორები და ინფორმაცია პოპულარული თამაშების შესახებ."/>
-
-<div className="mt-10">
-<TwoBigCardLayout card={gg} />
-</div> 
-
-
-<Newslayout sectionTitle="უახლესი" database={newsPosts}/>
-
-</section>
-
+      <section className="lg:ml-33 max-w-420 mb-5 px-5">
+        <NewsTitleLayout title="ახალი ამბები" desc="ყველაფერი, რაც გეიმერს აინტერესებს — ახალი ამბები, ტექნიკა, ჭორები და ინფორმაცია პოპულარული თამაშების შესახებ."/>
+        <div className="mt-10">
+          <TwoBigCardLayout card={gg} />
+        </div>
+        <Newslayout sectionTitle="უახლესი" database={newsPosts}/>
+      </section>
     </>
   )
 }
